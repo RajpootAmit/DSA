ee5cbda7f2cca7280254bb1c375294c34909d687
Amit Kumar Singh <61898886+RajpootAmit@users.noreply.github.com>
2022-05-27 14:09:00 +0530

    Create Print Reverse LinkedList.cpp

---
+++ LinkedList/Print Reverse LinkedList.cpp
@@ -0,0 +1,11 @@
+USING RECURSION.//
+
+void printReverse(Node *head)
+{
+    //Write your code here
+    if(head==NULL)
+        return;
+    printReverse(head->next);
+    cout<<head->data<<" ";
+    
+}
